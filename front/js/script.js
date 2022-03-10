643cd4453b0b9cfb7a7fd7d2ba059ef54ba05204
Meurot Alexandre <alexandre.meurot@tessi.fr>
2022-03-10 15:41:07 +0100

    création et affichage des nouveaux éléments dans le DOM

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,49 @@
 // liens vers API = http://localhost:3000/api/products
 
-const items = document.getElementById('items');
+const items = document.getElementById("items");
 
-let products = [];
+getProducts();
+displayProducts();
 
+async function getProducts() {
+    let data = await fetch("http://localhost:3000/api/products");
+    return await data.json();
+}
 
-// Fonction qui récupère la data de l'API pour la stocké dans productsData[]
+async function displayProducts() {
+    let result = await getProducts()
+    .then(function (dataProduct) {
+        const products = dataProduct;
+        console.log(products);
+    
+        for (let i = 0; i < products.length; i++) {
+      
+            // création élément lien + injecte data
+            let productLink = document.createElement("a");
+            items.appendChild(productLink);
+            productLink.href = `product.html?id=${dataProduct[i]._id}`;
+            
+            // création élément article
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
 
-const fetchProducts = async () => {
-    await fetch('http://localhost:3000/api/products')
-        .then((res) => res.json())
-        .then((data) => {
-            products = data;
-            console.log(data[0]);
-        })
-};
+            // création élément img + injecte data
+            let productImg = document.createElement('img');
+            productArticle.appendChild(productImg);
+            productImg.src = dataProduct[i].imageUrl;
+            productImg.alt = dataProduct[i].altTxt;
 
-fetchProducts();
+            // création élement h3 + injecte data
+            let productName = document.createElement('h3');
+            productArticle.appendChild(productName);
+            productName.classList.add('productName');
+            productName.innerHTML = dataProduct[i].name;
 
-
-
+            // création élément p + injecte data
+            let productDescription = document.createElement('p');
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add('productDescription');
+            productDescription.innerHTML = dataProduct[i].description;
+        }
+    });
+}
