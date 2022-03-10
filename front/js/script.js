88aad44eb3b22de1aeb9dd793be7f045560d262f
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-03-10 08:00:32 +0100

    ajout du fetch permettant de récupérer l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,22 @@
+// liens vers API = http://localhost:3000/api/products
+
+const items = document.getElementById('items');
+
+let products = [];
+
+
+// Fonction qui récupère la data de l'API pour la stocké dans productsData[]
+
+const fetchProducts = async () => {
+    await fetch('http://localhost:3000/api/products')
+        .then((res) => res.json())
+        .then((data) => {
+            products = data;
+            console.log(data[0]);
+        })
+};
+
+fetchProducts();
+
+
 
