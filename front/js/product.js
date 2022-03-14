a4f6a5ba53f756836bf9131f5ef606269c232111
Meurot Alexandre <alexandre.meurot@tessi.fr>
2022-03-14 15:08:36 +0100

    ajout fonction qui envoie le(s) produit(s) au Local Storage

--- front/js/product.js
+++ front/js/product.js
@@ -4,22 +4,23 @@ const title = document.getElementById('title');
 const price = document.getElementById('price');
 const description = document.getElementById('description');
 const selectColors = document.getElementById('colors');
+const quantity = document.getElementById('quantity');
+const btnAddToCart = document.getElementById('addToCart');
 
-
+// ---------------------------------------------------------
 // Méthode pour récupérer l'Id de l'URL de la page actuelle
 
 let url = window.location.href;
 let urlProduct = new URL(url);
 var search_params = new URLSearchParams(urlProduct.search); 
-
-if(search_params.has('id')) {
-	var id = search_params.get('id');
-}
+var id = search_params.get('id');
 // ---------------------------------------------------------
 
 getProduct();
 
+// ---------------------------------------------------------
 // fonction qui récupère les données de l'API => Json
+
 function getProduct() {
     fetch("http://localhost:3000/api/products/" + id)
     .then((data) => {
@@ -37,7 +38,9 @@ function getProduct() {
     })
 }
 
+// ---------------------------------------------------------
 // fonction qui affiche le produit dans le DOM
+
 function displayProduct(product) {
     
     // création de l'élément et affichage de l'image du produit dans le DOM
@@ -62,5 +65,32 @@ function displayProduct(product) {
         optionColors.value = colors;
         optionColors.innerHTML = colors;
     }
-}
+};
 
+// ---------------------------------------------------------
+// fonction qui envoie le(s) produit(s) au Local Storage
+
+btnAddToCart.addEventListener('click', (e) => {
+
+    // variables contenants couleurs et quantité du produit
+    let selectedColor = selectColors.value;
+    let selectedQuantity = quantity.value;
+    
+    // objet contenant toutes les infos du produit selectionné
+    let selectedProduct = {
+        idProduct : id,
+        nameProduct : product.name,
+        colorProduct : selectedColor,
+        quantityProduct : selectedQuantity,
+        priceProduct : product.price,
+        descritpionProduct : product.description,
+        imgProduct : product.imageUrl,
+        altImgProduct : product.altTxt
+    };
+
+    localStorageProducts = [];
+    localStorageProducts.push(selectedProduct);
+    localStorage.setItem('product', JSON.stringify(localStorageProducts));
+    console.table(localStorageProducts)
+
+})
