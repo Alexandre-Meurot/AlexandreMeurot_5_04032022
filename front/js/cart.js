65457731466628afa990e86be2efbdc1cba4c39d
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-03-20 16:22:00 +0100

    Ajout fonction qui récupère les données du local storage + ccondition si panier est vide

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,18 @@
+const cartItem = document.getElementById('cart__items');
+
 // Initialisation du localStorage
 let itemLocalStorage = JSON.parse(localStorage.getItem("item"));
 console.table(itemLocalStorage);
+
+// fonction qui récupère les données du localStorage
+function getCart() {
+
+    // Si le panier est vide
+    if (itemLocalStorage === null || itemLocalStorage == 0) {
+        let emptyCart = document.createElement('p');
+        cartItem.appendChild(emptyCart);
+        emptyCart.textContent = 'Votre panier est vide'
+    }
+}
+
+getCart();
