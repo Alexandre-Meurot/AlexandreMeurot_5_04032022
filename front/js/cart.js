245da17319129882b5e9f60be0887849f16b9cc8
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-03-20 16:52:27 +0100

    Ajout d'une boucle + création et insertion de l'élément article

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,9 +9,23 @@ function getCart() {
 
     // Si le panier est vide
     if (itemLocalStorage === null || itemLocalStorage == 0) {
+        
         let emptyCart = document.createElement('p');
         cartItem.appendChild(emptyCart);
         emptyCart.textContent = 'Votre panier est vide'
+
+    } else {
+        
+        for (let item in itemLocalStorage) {
+
+            // création et insertion de l'élément article + les attributs
+            let productArticle = document.createElement('article');
+            cartItem.appendChild(productArticle);
+            productArticle.className = 'cart__item';
+            productArticle.setAttribute('data-id', itemLocalStorage[item].idItem);
+            productArticle.setAttribute('data-color', itemLocalStorage[item].colorItem);
+
+        }
     }
 }
 
